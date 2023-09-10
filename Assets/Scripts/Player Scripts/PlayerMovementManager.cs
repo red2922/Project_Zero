1e90334cb34670b1b92e4ac1d3633ff1ce34b606
red2922 <inthavongsaj@lopers.unk.edu>
2023-09-10 02:03:18 -0500

    Began Programming Basic Input Manager

---
+++ Assets/Scripts/Player Scripts/PlayerMovementManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovementManager : MonoBehaviour
+{
+    //Variables
+    private PlayerControls playerMovements;
+    private PlayerControls.MovementActions basicMovements;
+
+    private PlayerMovements move;
+
+    //Functions
+
+    //Essentially being able to disable vs enable movement
+    private void OnEnable()
+    {
+        basicMovements.Enable();
+    }
+
+    private void OnDisable()
+    {
+        basicMovements.Disable();
+    }
+
+    //Essentially Main/What is going to be player in game
+    void Awake()
+    {
+        playerMovements = new PlayerControls();
+        basicMovements = playerMovements.Movement;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+  
+
+
+
+
+
+}
---
+++ Assets/Scripts/Player Scripts/PlayerMovements.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour
+{
+    //Variables
+    private CharacterController Player;
+    private Vector3 playerVelocity;
+
+    public float speed = 8f;
+
+    //Functions
+
+
+    
+    public void GetMove(Vector2 input) // Get Player inputs (Controller or keyboard)
+    {
+        Vector3 moveDirection = Vector3.zero;
+
+
+
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
