b3a916c9e00fa53ead8927f3ea8ab728a511bd78
g-olsen13 <garret1237@gmail.com>
2023-09-11 19:41:50 -0500

    adding basic sceneloader
    provided a starting script to load scenes(WIP). Need to load into buttons onClick()/need to fix repeat issues

---
+++ Assets/Garret/Scene_Loader.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene();
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+//
+//    }
+}
