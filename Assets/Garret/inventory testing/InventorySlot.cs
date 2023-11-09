47cf9839238805aa4c9aae07991c7505abc89bdb
red2922 <inthavongsaj@lopers.unk.edu>
2023-11-09 13:53:47 -0600

    Pain

--- Assets/Garret/inventory testing/InventorySlot.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class InventorySlot : MonoBehaviour
-{
-    public Image iconImage;
-    public Button slotButton;
-}
